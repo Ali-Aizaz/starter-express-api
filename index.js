f156f12eb45d3c4d7b5c788247624e969d0646cb
Ali-Aizaz <50923380+Ali-Aizaz@users.noreply.github.com>
2022-07-24 18:37:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,72 @@
 const express = require('express')
 const app = express()
+const bodyparser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const jwksClient = require("jwks-rsa")
+
+class ErrorResponse extends Error {
+  constructor(message, errorCode) {
+    super(message);
+    this.message = message;
+    this.code = errorCode;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+app.use(bodyparser.json({ limit: "50mb" }));
+
+const appleLogin = async (req, res, next) => {
+  const { provider, response } = req.body;
+
+  if (provider === "apple") {
+    //validate apple signin
+    const { identityToken, user } = response.response;
+    const jwToken = jwt.decoded(identityToken, { complete: true });
+    const kid = jwToken.header.kid;
+
+    const appleKey = await getAppleSigninKey(kid);
+    if (!appleKey) {
+      return next(new ErrorResponse("Apple Server Error", 500));
+    }
+
+    const payload = await verifyJWT(identityToken, appleKey);
+    if (!payload) {
+      return next(new ErrorResponse("Apple Server Error", 500));
+    }
+    res.status(200).json({ "Sign in with apple success ": payload });
+  }
+};
+
+const verifyJWT = (token, appleKey) => {
+  return new Promise((resolve) => {
+    jwt.verify(token, appleKey, (err, payload) => {
+      if (err) {
+        console.error(err);
+        return resolve(null);
+      }
+      resolve(payload);
+    });
+  });
+};
+
+const client = jwksClient({
+  jwksUri: "https://appleid.apple.com/auth/keys",
+});
+
+//gets the public key from apple
+const getAppleSigninKey = (kid) => {
+  return new Promise((resolve) => {
+    client.getSigningKey(kid, (err, key) => {
+      if (err) {
+        console.error(err);
+        return resolve(null);
+      }
+      const signingKey = key.getPublicKey();
+      resolve(signingKey);
+    });
+  });
+};
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
